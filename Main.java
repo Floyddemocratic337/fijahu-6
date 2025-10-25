dab5dd39b518e7000ba83744c2e1b943e0e4f5fa
zemguimaraes <josemaria_vasconcelosguimaraes@hotmail.com>
2025-10-25 16:34:16 +0000

    Initial commit: java project

---
+++ Main.java
@@ -0,0 +1,19 @@
+/**
+ * fijahu-6 - Java Implementation
+ * A simple Hello World program
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Welcome to fijahu-6");
+        
+        // Simple calculation
+        int[] numbers = {1, 2, 3, 4, 5};
+        int total = 0;
+        for (int num : numbers) {
+            total += num;
+        }
+        System.out.println("Sum of numbers: " + total);
+    }
+}
